5b5634d18cd16d724ad7098462a36dae97e59bc8
Artur <henriqueacg@gmail.com>
2020-08-14 17:28:56 -0300

    business rules on srtHalf

--- classes/Utils.rb
+++ classes/Utils.rb
@@ -13,7 +13,15 @@ class Utils
     
     def srtHalf(str)
         if str.is_a? String
-            return str[0..(str.length/2-1).floor]
+            const = firstHalf?
+            
+            half = (str.length/2).floor - 1
+
+            init = const * (half+1)
+            final = (const == 0 ? half : str.length - 1 )
+
+            puts "[half=#{half}, init=#{init}, final=#{final}, length=#{str.length}, const=#{const}]"
+            return str[init..final]
         else
             puts "Parametro inválido.\nPasse uma string como parametro"
             newStr = gets.chomp
@@ -42,4 +50,20 @@ class Utils
             puts 'Data nao válida!'
         end
     end
+
+    private
+        def firstHalf?
+            puts 'Você deseja imprimir a primeira metade da String ou a segunda?'
+            puts '[1] - Primeira metade'
+            puts '[2] - Segunda metade'
+            response = gets.chomp
+
+            if response == '1' || response == '2'
+                return (response == '1' ? 0 : 1)
+            else
+                puts 'Resposta inválida.'
+                puts "\n"
+                firstHalf?
+            end
+        end
 end
